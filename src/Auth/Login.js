671426e06bf0e0a4894ddd3437e2eab5647c2bce
daoyi7 <kawhichina@gmail.com>
2017-12-27 12:00:36 +0800

    update

--- src/Auth/Login.js
+++ src/Auth/Login.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import '../static/login.css'
 
 export default class Login extends Component {
   render () {
     return (
       <div className="login">
-        <h2>Login Page</h2>
+        <div className="login-box">
+          <h2>欢迎登录</h2>
+        </div>
       </div>
     )
   }
